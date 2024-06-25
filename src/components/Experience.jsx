96a8203581ef3b19a32b1965c18305187c054dc5
freab mesfin <fremesfin1@gmail.com>
2024-06-26 01:50:03 +0300

    add bloom

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,35 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { ScrollControls, useScroll } from "@react-three/drei";
+import { ScrollControls, useScroll, useGLTF } from "@react-three/drei";
+import * as THREE from "three";
 import gsap from "gsap";
-import Axum from "./axum";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
+
+function Axum() {
+  const group = useRef();
+  const { nodes } = useGLTF("/axum.glb");
+
+  return (
+    <group ref={group} position={[0, -1.5, 0]}>
+      <mesh
+        geometry={nodes.The_Obelisk_Of_Axum.geometry}
+        material={nodes.The_Obelisk_Of_Axum.material}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/axum.glb");
 
 function AnimatedAxum() {
   const ref = useRef();
   const scroll = useScroll();
   const { scene } = useThree();
+  const [glow, setGlow] = useState(false);
+
+  useEffect(() => {
+    scene.background = new THREE.Color(1, 1, 1); // Initial background color white
+  }, [scene]);
 
   useFrame(() => {
     const section = Math.floor(scroll.offset * 5);
@@ -19,22 +41,20 @@ function AnimatedAxum() {
         gsap.to(ref.current.position, { x: 0, y: 0, z: 0, duration: 2 });
         gsap.to(ref.current.scale, { x: 1, y: 1, z: 1, duration: 0.5 });
         gsap.to(scene.background, { r: 1, g: 1, b: 1, duration: 0.5 });
+        setGlow(false);
         break;
       case 1: // Section 2
-        gsap.to(ref.current.rotation, {
-          x: 0,
-          y: Math.PI / 1.7,
-          z: 0,
-          duration: 1.5
-        });
+        gsap.to(ref.current.rotation, { y: Math.PI / 2, duration: 1.5 });
         gsap.to(ref.current.position, { x: -2, y: 0, z: 0, duration: 0.5 });
         gsap.to(ref.current.scale, { x: 1.5, y: 1.5, z: 1.5, duration: 0.5 });
+        setGlow(false);
         break;
       case 2: // Section 3
         gsap.to(ref.current.rotation, { y: 0, duration: 0.5 });
         gsap.to(ref.current.position, { x: 0, y: 0, z: 0, duration: 0.5 });
         gsap.to(ref.current.scale, { x: 1, y: 1, z: 1, duration: 0.5 });
         gsap.to(scene.background, { r: 0, g: 0, b: 0, duration: 0.5 });
+        setGlow(true);
         break;
       case 3: // Section 4
         const rotationY = progress * Math.PI * 2;
@@ -46,11 +66,13 @@ function AnimatedAxum() {
           z: scale,
           duration: 0.2
         });
+        setGlow(true);
         break;
       case 4: // Section 5
         gsap.to(ref.current.position, { x: 0, y: 0, z: 0, duration: 0.5 });
         gsap.to(ref.current.scale, { x: 1, y: 1, z: 1, duration: 0.5 });
         gsap.to(scene.background, { r: 1, g: 1, b: 1, duration: 0.5 });
+        setGlow(false);
         break;
       default:
         break;
@@ -60,6 +82,15 @@ function AnimatedAxum() {
   return (
     <group ref={ref}>
       <Axum />
+      {glow && (
+        <EffectComposer>
+          <Bloom
+            intensity={1.5}
+            luminanceThreshold={0.2}
+            luminanceSmoothing={0.1}
+          />
+        </EffectComposer>
+      )}
     </group>
   );
 }
